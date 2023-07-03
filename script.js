caeb075f78c73e910ff496b08e6a36d585a607c9
Nancysuccess <131716739+NancySuccesO@users.noreply.github.com>
2023-07-03 11:57:42 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 // console.log()
  const togglebutton = document.getElementsByClassName("toggle-button")[0]
  const navbarlinks = document.getElementsByClassName("navbarlinks")[0]
- const ulia = document.getElementsByClassName("ulia")
+ // const ulia = document.getElementsByClassName("ulia")
 
 
  togglebutton.addEventListener("click", ()=>{
     navbarlinks.classList.toggle("active");
-    ulia.classList.toggle("active");
+    // ulia.classList.toggle("active");
  })
 
 
